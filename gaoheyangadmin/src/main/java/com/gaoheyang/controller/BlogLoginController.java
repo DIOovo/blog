c5c5af77b641508a52a8ce3e91e9afed6008dbce
DIOovo <99567011+DIOovo@users.noreply.github.com>
2023-08-25 11:18:30 +0800

    Add files via upload

---
+++ gaoheyangadmin/src/main/java/com/gaoheyang/controller/BlogLoginController.java
@@ -0,0 +1,20 @@
+package com.gaoheyang.controller;
+
+import com.gaoheyang.domain.ResponseResult;
+import com.gaoheyang.domain.entity.User;
+import com.gaoheyang.service.BlogLoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogLoginController {
+    @Autowired
+    private BlogLoginService blogLoginService;
+
+    @PostMapping("/login")
+    public ResponseResult login(@RequestBody User user){
+        return blogLoginService.login(user);
+    }
+}
